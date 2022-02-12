f9e947598afd7c75c060686ad00d4c183b324662
JuliaTomchuk <79898598+JuliaTomchuk@users.noreply.github.com>
2022-02-12 22:16:53 +0300

    Update Task05.java

--- src/by/jepam/module01/linear/Task05.java
+++ src/by/jepam/module01/linear/Task05.java
@@ -1,13 +1,16 @@
 package by.jepam.module01.linear;
 
-import java.time.LocalTime;
-
 /*Дано натуральное число T, которое обозначает длительность прошедшего времени в секундах.
 Вывести данное значение длительность в часах, минутах и секундах в следующей форме HHч.ММмин.SSсек.*/
 public class Task05 {
 	public static void main(String args[]) {
 		int s = 10897;
-		System.out.println(LocalTime.ofSecondOfDay(s));
+		int hour = s / 3600;
+		int minute = (s - (3600 * hour)) / 60;
+		int second = (s - (3600 * hour) - (60 * minute));
+
+		System.out.println(hour + "ч." + minute + "мин." + second + "сек.");
 	}
 
 }
+
